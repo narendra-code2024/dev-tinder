aa033bda58666e827bef0565ab08f61b9fe15ccb
narendra-code2024 <im.narendra.k@gmail.com>
2025-06-08 16:10:43 +0530

    added validator

--- src/models/user.js
+++ src/models/user.js
@@ -1,48 +1,66 @@
 const mangoose = require("mongoose");
+const validator = require("validator");
 
 const { Schema } = mangoose;
 
-const userSchema = new Schema({
-	firstName: {
-		type: String,
-		required: true,
-		minlength: 3,
-		maxlength: 50,
-	},
-	lastName: {
-		type: String,
-	},
-	emailId: {
-		type: String,
-		lowercase: true,
-		trim: true,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	age: {
-		type: Number,
-		min: 18,
-	},
-	gender: {
-		type: String,
-		validate(value) {
-			if (value !== "male" && value !== "female" && value !== "other") {
-				throw new Error("Invalid gender");
-			}
+const userSchema = new Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+			minlength: 3,
+			maxlength: 50,
+		},
+		lastName: {
+			type: String,
+		},
+		emailId: {
+			type: String,
+			lowercase: true,
+			trim: true,
+			required: true,
+			unique: true,
+			validate(value) {
+				if (!validator.isEmail(value)) {
+					throw new Error("Invalid email");
+				}
+			},
+		},
+		password: {
+			type: String,
+			required: true,
+			validate(value) {
+				if (!validator.isStrongPassword(value)) {
+					throw new Error("Invalid password");
+				}
+			},
+		},
+		age: {
+			type: Number,
+			min: 18,
+		},
+		gender: {
+			type: String,
+			validate(value) {
+				if (
+					value !== "male" &&
+					value !== "female" &&
+					value !== "other"
+				) {
+					throw new Error("Invalid gender");
+				}
+			},
+		},
+		about: {
+			type: String,
+		},
+		skills: {
+			type: [String],
 		},
 	},
-	about: {
-		type: String,
-	},
-	skills: {
-		type: [String],
+	{
+		timestamps: true,
 	}
-}, {
-	timestamps: true
-});
+);
 
 module.exports = mangoose.model("User", userSchema);
