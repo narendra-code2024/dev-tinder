7cc21aee5ce085a723d9306c6282dc34fdca376b
narendra-code2024 <im.narendra.k@gmail.com>
2025-06-15 00:53:27 +0530

    reset password api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const { userAuth } = require("../middlewares/auth");
 const { validateEditData } = require("../utils/validations");
 
@@ -28,6 +29,33 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
 
 		res.json({
 			message: `${loggedInUser.firstName}, your profile updated successfully`,
+			data: loggedInUser,
+		});
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+});
+
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+	try {
+		const loggedInUser = req.user;
+
+		const { currentPassword, newPassword } = req.body;
+
+		const isPasswordValid = await loggedInUser.comparePassword(
+			currentPassword
+		);
+
+		if (!isPasswordValid) {
+			throw new Error("Current password is incorrect");
+		}
+
+		loggedInUser.password = await bcrypt.hash(newPassword, 10);
+
+		await loggedInUser.save();
+
+		res.json({
+			message: `${loggedInUser.firstName}, your password updated successfully`,
 			data: loggedInUser,
 		});
 	} catch (err) {
